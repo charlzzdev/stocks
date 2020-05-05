f56824b53c5098e7d81c6eb158c64d40d69ad0a1
Charles <ellerkaresz@gmail.com>
2020-05-05 20:45:32 +0200

    add ability to search by stock symbols

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,36 @@
+import { useState, useEffect } from 'react';
+
 const Index = () => {
+  const [stockSymbol, setStockSymbol] = useState('GOOGL');
+  const [stockData, setStockData] = useState({});
+
+  useEffect(() => {
+    fetch(`https://financialmodelingprep.com/api/v3/quote/${stockSymbol}`)
+      .then(res => res.json())
+      .then(data => {
+        setStockData(data[0] || {})
+      });
+  }, [stockSymbol]);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setStockSymbol(e.target[0].value.toUpperCase());
+    e.target[0].value = '';
+  }
+
   return (
-    <div>index</div>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="stock-symbol">Stock Symbol: </label>
+        <input type="text" id="stock-symbol" defaultValue={stockSymbol} />
+        <button>Search</button>
+      </form>
+      {
+        stockData.symbol ? <div>
+          {stockData.symbol}: ${stockData.price.toFixed(2)} ({stockData.changesPercentage.toFixed(2)}%)
+        </div> : `${stockSymbol} was not found.`
+      }
+    </div>
   )
 }
 
